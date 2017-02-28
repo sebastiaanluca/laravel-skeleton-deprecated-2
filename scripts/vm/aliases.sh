c9dd47de5823dfead6f87e8be5a4854ac52520f4
Sebastiaan Luca <hello@sebastiaanluca.com>
2017-02-28 15:08:16 +0100

    Upgrade VM setup
    Signed-off-by: Sebastiaan Luca <hello@sebastiaanluca.com>

--- scripts/vm/aliases.sh
+++ scripts/vm/aliases.sh
@@ -1,18 +1,33 @@
 #!/bin/bash
 
-# Aliases
+alias ..="cd .."
+alias ...="cd ../.."
+
 alias c='composer'
 alias co='composer'
 alias cr='composer require'
 alias cda='composer dumpautoload'
-alias a='php artisan'
-alias art='php artisan'
-alias arti='php artisan'
-alias pa='php artisan'
-alias phpa='php artisan'
+alias a=artisan
+alias art=artisan
+alias arti=artisan
+alias pa=artisan
+alias phpa=artisan
 alias y='yarn'
 alias yr='yarn run'
+
 alias test='vendor/bin/phpunit'
+alias phpspec='vendor/bin/phpspec'
+alias phpunit='vendor/bin/phpunit'
+alias serve=serve-laravel
+
+alias xoff='sudo phpdismod -s cli xdebug'
+alias xon='sudo phpenmod -s cli xdebug'
+
+function artisan() {
+    php artisan "$@"
+}
+
+# Elasticsearch
 
 alias es-list-indices='curl -XGET http://localhost:9200/_cat/indices?v'
 
@@ -22,6 +37,19 @@ es-delete-index() {
 
 es-list-documents() {
     curl -XGET localhost:9200/$1/_search?pretty=true&q=*:*
+}
+
+# Publicly share VM
+
+function share() {
+    if [[ "$1" ]]
+    then
+        ngrok http -host-header="$1" 80
+    else
+        echo "Error: missing required parameters."
+        echo "Usage: "
+        echo "  share domain"
+    fi
 }
 
 # Automatically navigate into project dir on session start
--- scripts/vm/setup/mailhog.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo ">> Configuring Mailhog"
-
-sudo systemctl daemon-reload
-sudo systemctl enable mailhog.service
-sudo systemctl start mailhog.service
