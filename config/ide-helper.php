c983b29670722a1ad99fb8325bd72b790ff1d845
Sebastiaan Luca <hello@sebastiaanluca.com>
2017-03-24 13:21:06 +0100

    Tweak IDE helper
    Signed-off-by: Sebastiaan Luca <hello@sebastiaanluca.com>

--- config/ide-helper.php
+++ config/ide-helper.php
@@ -35,7 +35,7 @@ return [
     |
     */
 
-    'include_helpers' => false,
+    'include_helpers' => true,
 
     'helper_files' => [
         base_path() . '/vendor/laravel/framework/src/Illuminate/Support/helpers.php',
